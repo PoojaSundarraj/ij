66fc2fb8e55701b5dd0d84279fc729d61c2afb5e
PoojaSundarraj <poojasundarraj23@gmail.com>
2017-06-12 12:26:33 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class sample
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String n=s.next();
+if(n="a"||n=="e"||n=="i"||n=="o"||n=="u")
+{
+System.out.println("vowel");
+}
+else
+{
+System.out.print("constant");
+}
+}
+}
